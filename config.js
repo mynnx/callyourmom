45e1141e4dfd466936351e5f986ddf8537cee20e
Mark Everett <mark.everett@rackspace.com>
2012-05-13 20:39:27 -0700

    Adding sample config

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.rtm = {
+  apiKey: "API KEY",
+  secret: "SECRET",
+  list: "invoke RTM.getLists to get ID"
+};
+
+exports.gv = {
+  email: "something@gmail.com",
+  password: "123abc",
+  number: "8675309"
+};
+
+exports.misc = {
+  pollDelay: 20
+};
